605fad4ab503cd7fa6793d255a6cdc490c87304c
王宁 <vipning@qq.com>
2020-04-23 19:00:13 +0800

    commit

--- NingMonitor/Forms/DisplayForm.Designer.cs
+++ NingMonitor/Forms/DisplayForm.Designer.cs
@@ -33,64 +33,93 @@
             this.instrument12 = new MonitorControlsLibrary.Instrument1.Instrument1();
             this.instrument11 = new MonitorControlsLibrary.Instrument1.Instrument1();
             this.tempControl1 = new MonitorControlsLibrary.TempControl.TempControl();
+            this.instrument21 = new MonitorControlsLibrary.Instrument2.Instrument2();
+            this.tempControl2 = new MonitorControlsLibrary.TempControl.TempControl();
             this.SuspendLayout();
             // 
             // instrument14
             // 
             this.instrument14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.instrument14.Location = new System.Drawing.Point(539, 348);
+            this.instrument14.Location = new System.Drawing.Point(698, 671);
+            this.instrument14.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.instrument14.Name = "instrument14";
-            this.instrument14.Size = new System.Drawing.Size(278, 148);
+            this.instrument14.Size = new System.Drawing.Size(287, 184);
             this.instrument14.TabIndex = 4;
             this.instrument14.Text = "instrument14";
             // 
             // instrument13
             // 
             this.instrument13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.instrument13.Location = new System.Drawing.Point(231, 348);
+            this.instrument13.Location = new System.Drawing.Point(236, 671);
+            this.instrument13.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.instrument13.Name = "instrument13";
-            this.instrument13.Size = new System.Drawing.Size(278, 148);
+            this.instrument13.Size = new System.Drawing.Size(287, 184);
             this.instrument13.TabIndex = 3;
             this.instrument13.Text = "instrument13";
             // 
             // instrument12
             // 
             this.instrument12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.instrument12.Location = new System.Drawing.Point(539, 158);
+            this.instrument12.Location = new System.Drawing.Point(698, 458);
+            this.instrument12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.instrument12.Name = "instrument12";
-            this.instrument12.Size = new System.Drawing.Size(278, 148);
+            this.instrument12.Size = new System.Drawing.Size(287, 184);
             this.instrument12.TabIndex = 2;
             this.instrument12.Text = "instrument12";
             // 
             // instrument11
             // 
             this.instrument11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.instrument11.Location = new System.Drawing.Point(231, 158);
+            this.instrument11.Location = new System.Drawing.Point(236, 458);
+            this.instrument11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.instrument11.Name = "instrument11";
-            this.instrument11.Size = new System.Drawing.Size(278, 148);
+            this.instrument11.Size = new System.Drawing.Size(287, 184);
             this.instrument11.TabIndex = 1;
             this.instrument11.Text = "instrument11";
             // 
             // tempControl1
             // 
             this.tempControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.tempControl1.Location = new System.Drawing.Point(106, 43);
+            this.tempControl1.Location = new System.Drawing.Point(22, 13);
+            this.tempControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tempControl1.Name = "tempControl1";
-            this.tempControl1.Size = new System.Drawing.Size(59, 506);
+            this.tempControl1.Size = new System.Drawing.Size(47, 397);
             this.tempControl1.TabIndex = 0;
             this.tempControl1.Text = "tempControl1";
             // 
+            // instrument21
+            // 
+            this.instrument21.BackColor = System.Drawing.Color.Black;
+            this.instrument21.Location = new System.Drawing.Point(135, -11);
+            this.instrument21.Name = "instrument21";
+            this.instrument21.Size = new System.Drawing.Size(946, 476);
+            this.instrument21.TabIndex = 5;
+            this.instrument21.Text = "instrument21";
+            // 
+            // tempControl2
+            // 
+            this.tempControl2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tempControl2.Location = new System.Drawing.Point(110, 13);
+            this.tempControl2.Margin = new System.Windows.Forms.Padding(4);
+            this.tempControl2.Name = "tempControl2";
+            this.tempControl2.Size = new System.Drawing.Size(47, 397);
+            this.tempControl2.TabIndex = 6;
+            this.tempControl2.Text = "tempControl2";
+            // 
             // DisplayForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1080, 1061);
+            this.ClientSize = new System.Drawing.Size(1058, 1410);
+            this.Controls.Add(this.tempControl2);
             this.Controls.Add(this.instrument14);
             this.Controls.Add(this.instrument13);
             this.Controls.Add(this.instrument12);
             this.Controls.Add(this.instrument11);
             this.Controls.Add(this.tempControl1);
+            this.Controls.Add(this.instrument21);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "DisplayForm";
             this.Text = "DisplayForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DisplayForm_FormClosing);
@@ -109,5 +138,7 @@
         private MonitorControlsLibrary.Instrument1.Instrument1 instrument12;
         private MonitorControlsLibrary.Instrument1.Instrument1 instrument13;
         private MonitorControlsLibrary.Instrument1.Instrument1 instrument14;
+        private MonitorControlsLibrary.Instrument2.Instrument2 instrument21;
+        private MonitorControlsLibrary.TempControl.TempControl tempControl2;
     }
 }
